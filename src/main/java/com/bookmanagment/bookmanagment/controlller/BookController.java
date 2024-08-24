3aa55649ce6eb64c41b4480d5d9543603380fafe
anjalikumari930 <anjalikumari93045@gmail.com>
2024-08-24 15:26:05 +0530

    Revert "Added comment in book controller"
    This reverts commit ed097298819ee5f457819d4223177fc890475a75.

--- src/main/java/com/bookmanagment/bookmanagment/controlller/BookController.java
+++ src/main/java/com/bookmanagment/bookmanagment/controlller/BookController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-//i love anjali
 @RestController
 @RequestMapping("/books")
 public class BookController {
