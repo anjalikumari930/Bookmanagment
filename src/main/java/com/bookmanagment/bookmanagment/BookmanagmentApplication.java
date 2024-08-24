c1e611628997e823df1e00eb2bf0ab622b89b64f
anjalikumari930 <anjalikumari93045@gmail.com>
2024-08-24 15:33:25 +0530

    added comment to bookmanagmentapplication

--- src/main/java/com/bookmanagment/bookmanagment/BookmanagmentApplication.java
+++ src/main/java/com/bookmanagment/bookmanagment/BookmanagmentApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookmanagmentApplication {
-
+	// cutie
 	public static void main(String[] args) {
 		SpringApplication.run(BookmanagmentApplication.class, args);
 	}
